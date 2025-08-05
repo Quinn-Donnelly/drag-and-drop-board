0604dc20b8f4f7083a95546170b7ee03da0bbbc2
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-08-05 07:39:28 -0400

    feat: shop disables dragging if too expensive
    - Likely need to figure out how to clean up all this casting

--- scene/shop/shop.gd
+++ scene/shop/shop.gd
@@ -5,9 +5,11 @@ extends PieceGrid
 @export var gameManager: GameManager
 #TODO: refactor this to be a spawner or something
 @export var pieceMover: PieceMover
+@export var productionManager: ProductionManager
 
 func _ready() -> void:
 	gameManager.connect("start_game", self._on_game_start)
+	productionManager.connect("current_production_update", self._on_resource_change)
 	super._ready()
 
 func setRandom() -> void: 
@@ -21,6 +23,14 @@ func createAndSet(location: Vector2i, workerInfo: WorkerInfo) -> void:
 	pieceMover.startListening(item)
 	item.global_position = get_global_tile_placement_position(location)
 	add_piece(location, item)
+
+func toggleAffordableUnits(currentGold: int) -> void:
+	for unit in grid.values():
+		if unit:
+			(unit as Piece).drag_and_drop.enabled = (unit as Piece).workerInfo.unitStats.cost < currentGold
+
+func _on_resource_change(currentResources: ResourceProduction):
+	toggleAffordableUnits(currentResources.gold)
 
 func _on_game_start() -> void:
 	setRandom()
