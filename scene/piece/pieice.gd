453914a9ed0ef0b6ba1ccd5f72ffdc57c615b94e
Quintin Donnelly <quinndonnelly22@gmail.com>
2025-08-08 07:34:18 -0400

    Feature/unit system (#10)
    * feat: adding ability to have different units
    
    - had to dynamically load the sprites
    
    * chore: cleaning up extra export confuses where stats come from

--- scene/piece/pieice.gd
+++ scene/piece/pieice.gd
@@ -4,11 +4,15 @@ extends Area2D
 
 
 @export var workerInfo: WorkerInfo
-@export var production: ResourceProduction
+var production: ResourceProduction
 @onready var drag_and_drop = $DragAndDrop
+@onready var sprite = $Sprite2D
 
 func _ready() -> void:
 	self.production = workerInfo.unitStats.production
+	if self.workerInfo.unitSprite:
+		sprite.texture = self.workerInfo.unitSprite
+	
 
 func getYield() -> ResourceProduction:
 	return production
